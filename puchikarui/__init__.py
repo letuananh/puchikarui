a25ec47cbfb8d328aecc357962a3f55ec46a0ff2
Le Tuan Anh <tuananh.ke@gmail.com>
2017-05-09 15:47:09 +0800

    Use relative import

--- puchikarui/__init__.py
+++ puchikarui/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from puchikarui.puchikarui import Schema, Execution, DataSource, Table
+from .puchikarui import Schema, Execution, DataSource, Table
 
 __all__ = ["Schema", "Execution", "DataSource", "Table"]
